4d6d813b1c773b58ede9737170eb5c893a85b1bf
guesss-aa <guesss-aa@mail.ru>
2024-11-25 15:49:14 +0300

    в script.js добавлены дата и время обмена

--- script.js
+++ script.js
@@ -14,12 +14,18 @@ document.getElementById("getRate").addEventListener("click", function () {
         .then(data => {
             const rate = data[crypto][currency];
             const cryptoName = crypto.charAt(0).toUpperCase() + crypto.slice(1);
-            const result = `1 ${cryptoName} = ${rate} ${currency.toUpperCase()}`;
-            document.getElementById("exchangeRate").innerText = result;
+
+            // Получение текущей даты и времени
+            const now = new Date();
+            const formattedDate = now.toLocaleDateString();
+            const formattedTime = now.toLocaleTimeString();
+
+            // Формирование текста результата
+            const result = `1 ${cryptoName} = ${rate} ${currency.toUpperCase()}<br>Дата и время: ${formattedDate}, ${formattedTime}`;
+            document.getElementById("exchangeRate").innerHTML = result;
         })
         .catch(error => {
             document.getElementById("exchangeRate").innerText = "Error fetching the exchange rate.";
             console.error("There was a problem with the fetch operation:", error);
         });
 });
-g
